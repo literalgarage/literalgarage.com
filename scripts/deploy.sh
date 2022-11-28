46668a36b0175b2fac626a8ac6ff5e11531a93a0
Dave <davepeck@gmail.com>
2022-11-28 10:11:01 -0800

    Initial commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Building literalgarage.com:"
+rm -rf dist/
+npm run build
+rm -f _site/deploy.sh
+
+echo "Syncing S3 bucket:"
+aws s3 sync dist/ s3://literalgarage.com --delete --size-only --acl public-read "$@" --profile literal-garage-web
+
+echo "Invalidating CloudFront distribution:"
+aws cloudfront create-invalidation --distribution-id E1P44WZPV25YC6 --profile literal-garage-web --paths "/*" "$@"
